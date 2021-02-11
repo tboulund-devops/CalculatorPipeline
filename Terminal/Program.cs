c5d75c7a64707d0c469f4cc60cc47a0675db24b4
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-02-11 12:58:20 +0100

    Fixed the main method

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -8,19 +8,7 @@ namespace Terminal
         static void Main(string[] args)
         {
             var calculator = new Calculator();
-            double start = Double.MaxValue;
-            var result = start * (start - 1);
-            var current = start - 2;
-            while (current != 0)
-            {
-                result *= current;
-                current--;
-            }
             
-            Console.WriteLine(result);
-            
-            
-            return;
             Console.WriteLine("Hello World!");
             Console.WriteLine(calculator.Add(1, 5, 8));
             Console.WriteLine(calculator.Subtract(10, 5, 2));
